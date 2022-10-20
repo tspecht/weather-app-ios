4464afa1ed218b4dbdb02583c40b9821a15c5bed
Tim Specht <tim.specht@reddit.com>
2022-10-20 07:53:16 -0600

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Tim Specht on 10/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
